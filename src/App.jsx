5aff4e76a7a3c554e7e9f9ba1e90bdd820e4fcc0
shahmeer <theshahmeerfsd@gmail.com>
2025-03-18 23:41:24 +0500

    add image carousel functionality with navigation controls

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,27 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const images = [
+    "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?auto=format&fit=crop&w=800&h=400",
+    "https://images.unsplash.com/photo-1477959858617-67f85cf4f1df?auto=format&fit=crop&w=800&h=400",
+    "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?auto=format&fit=crop&w=800&h=400",
+    "https://images.unsplash.com/photo-1557008075-7f2c5efa4cfd?auto=format&fit=crop&w=800&h=400",
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState();
+
+  const goTOBack = () => {
+    setCurrentIndex((preIndex) =>
+      preIndex === images.length - 1 ? 0 : preIndex + 1
+    );
+  };
+
+  const goToPreview = () => {
+    setCurrentIndex((preIndex) =>
+      preIndex === 0 ? images.length - 1 : preIndex - 1
+    );
+  };
+
   return <></>;
 }
 
